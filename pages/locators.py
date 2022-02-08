ec7e2b5cb14908267d7d940ce75435d8ea54b629
Mikhail <m.shishkin@novotorica.com>
2022-02-08 11:49:13 +0300

    update base page

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR,'#login_link')
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators():
     LOGIN_EMAIL_VALIDATION=(By.CSS_SELECTOR,'#id_login-username')
